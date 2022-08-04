a421e0f6c4841daf850ba8dc81e38ef08bdff272
naveen701526 <neonew8984@gmail.com>
2022-08-04 23:06:22 +0530

    new code added codechef

--- 0-1000 Beginner Level/DNASTRAND.py
+++ 0-1000 Beginner Level/DNASTRAND.py
@@ -1,7 +1,6 @@
-
 import sys
-# sys.stdout = open('./output.txt', 'w')
-# sys.stdin = open('./input.txt', 'r')
+sys.stdout = open('./output.txt', 'w')
+sys.stdin = open('./input.txt', 'r')
 
 
 def answer_finder():
@@ -23,4 +22,3 @@ test_cases = int(input())
 while test_cases:
     test_cases -= 1
     answer_finder()
-
