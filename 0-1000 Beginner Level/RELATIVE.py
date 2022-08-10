a442a9974b62107f6954e19eba992689eb538c29
naveen701526 <neonew8984@gmail.com>
2022-08-10 23:20:44 +0530

    new codes codechef 0-1000 rating

--- 0-1000 Beginner Level/RELATIVE.py
+++ 0-1000 Beginner Level/RELATIVE.py
@@ -1,17 +1,17 @@
+
 import sys
 sys.stdout = open('./output.txt', 'w')
 sys.stdin = open('./input.txt', 'r')
-import math
 
 
 def answer_finder():
-    k, n , w = [int(x) for x in input().split()]
-    facts = (w*(w+1))/2
-    val = k*facts
-    ans = val - n if (val -n) > 0 else 0
-    ans = math.ceil(ans)
-    print(ans)
-    
+    g, c = [int(x) for x in input().split()]
+    numerator = pow(c, 2)
+    denominator = 2*g
+    print(numerator//denominator)
 
 
-answer_finder()
+test_cases = int(input())
+while test_cases:
+    test_cases -= 1
+    answer_finder()
